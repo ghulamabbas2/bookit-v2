4a1ec68253adae995be117eb6a6cc47927d12888
Ghulam Abbas <ghulamabbas258@gmail.com>
2023-09-08 15:58:43 +0500

    fixed user side menu

--- components/layout/UserSidebar.tsx
+++ components/layout/UserSidebar.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 
 const UserSidebar = () => {
+  const pathname = usePathname();
+
   const menuItem = [
     {
       name: "Update Profile",
@@ -22,7 +25,7 @@ const UserSidebar = () => {
     },
   ];
 
-  const [activeMenuItem, setActiveMenuItem] = useState(menuItem[0].name);
+  const [activeMenuItem, setActiveMenuItem] = useState(pathname);
 
   const handleMenuItemClick = (menuItem: string) => {
     setActiveMenuItem(menuItem);
@@ -35,10 +38,10 @@ const UserSidebar = () => {
           key={index}
           href={menuItem.url}
           className={`fw-bold list-group-item list-group-item-action ${
-            activeMenuItem === menuItem.name ? "active" : ""
+            activeMenuItem === menuItem.url ? "active" : ""
           }`}
-          onClick={() => handleMenuItemClick(menuItem.name)}
-          aria-current={activeMenuItem === menuItem.name ? "true" : "false"}
+          onClick={() => handleMenuItemClick(menuItem.url)}
+          aria-current={activeMenuItem === menuItem.url ? "true" : "false"}
         >
           <i className={`${menuItem.icon} fa-fw pe-2`}></i> {menuItem.name}
         </Link>
